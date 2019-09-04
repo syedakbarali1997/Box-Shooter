2506b2b9cd2982b230b4fe30c2aef4dce374e76b
Syed Akbar <syedakbarali1997@gmail.com>
2019-09-04 16:28:06 +0500

    Added Moveable Targets/Player and Camera Settings/Shooting Projectiles/UI/Game Manager/Particles and SFX/Spawner/Animation of Targets

---
+++ Box Shooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,28 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doSpin)
+        {
+            //rotate gameObject along Y-Axis
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+
+        if (doMotion)
+        {
+            //move up and down over time
+            gameObject.transform.Translate(Vector3.up * motionMagnitude * Mathf.Cos(Time.timeSinceLevelLoad));
+        }
+    }
+}
